76abf01769bb12c410a0dcc03d792969f1eaac4f
andrewlarsen514 <andrew.larse514@gmail.com>
2017-01-18 15:52:55 -0600

    fix while loop

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ deploy_cluster() {
     primaryRunningCount=$( aws ecs describe-services --cluster TCFJCluster --services donorservice --query 'services[?serviceArn==`arn:aws:ecs:us-west-2:417615409974:service/donorservice`].[deployments[?status==`PRIMARY`].{runningCount:runningCount}]' --output text)
     echo "$primaryRunningCount"
 
-    while [[ $primaryRunningCount < 1 ]]; do
+    while [[ $primaryRunningCount -le 1 ]]; do
         #refactor this to
         primaryRunningCount=$( aws ecs describe-services --cluster TCFJCluster --services donorservice --query 'services[?serviceArn==`arn:aws:ecs:us-west-2:417615409974:service/donorservice`].[deployments[?status==`PRIMARY`].{runningCount:runningCount}]' --output text)
         echo "Waiting for stale deployments:"
@@ -50,8 +50,7 @@ deploy_cluster() {
         sleep 10
     done
 
-    echo "Service update took too long."
-    return 1
+    echo "Service updated succesfully"
 }
 
 make_task_def(){
